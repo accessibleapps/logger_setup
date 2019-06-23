22549ff060037eac06a5f629414826bb96800346
Christopher Toth <q.alpha@gmail.com>
2019-06-22 23:08:19 -0600

    Setup cleanup

--- setup.py
+++ setup.py
@@ -3,26 +3,28 @@ from setuptools import setup, find_packages
 
 install_requires = []
 if sys.version_info[0] < 3:
- install_requires.append('faulthandler==2.6')
+    install_requires.append("faulthandler==2.6")
 
 __version__ = 0.14
-__doc__ = """Collects log setup and handling from all projects into one central place."""
-__author__ = 'Q'
+__doc__ = (
+    """Collects log setup and handling from all projects into one central place."""
+)
+__author__ = "Christopher Toth"
 
 setup(
- name = 'logger_setup',
- version = __version__,
- author = __author__,
- author_email = 'q@q-continuum.net',
- description = __doc__,
- package_dir = {'logger_setup': 'logger_setup'},
- packages = find_packages(),
- install_requires = install_requires,
- classifiers = [
-  'Development Status :: 4 - beta',
-  'Intended Audience :: Developers',
-  'Programming Language :: Python',
-  'License :: OSI Approved :: MIT License',
-  'Topic :: Software Development :: Libraries',
- ],
+    name="logger_setup",
+    version=__version__,
+    author=__author__,
+    author_email="q@q-continuum.net",
+    description=__doc__,
+    package_dir={"logger_setup": "logger_setup"},
+    packages=find_packages(),
+    install_requires=install_requires,
+    classifiers=[
+        "Development Status :: 4 - beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries",
+    ],
 )
